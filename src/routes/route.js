9f4bac796a19fca1f8ccd850abb8737f66a62858
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-03 01:01:37 +0530

    assignment4

--- src/routes/route.js
+++ src/routes/route.js
@@ -20,10 +20,15 @@ router.get('/hello', function (req, res) {
     let firstElement = underscore.first(["s","a","c"]);
     console.log(firstElement);
     let arr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    let monthsArray = _.chunk(arr, 4);
+    let monthsArray = _.chunk(arr, 3);
     console.log(monthsArray);
     let oddArray = _.tail([1,3,5,7,9,11,13,15,17,19]);
     console.log(oddArray);
+    let numArray = _.union([4],[4,1],[5,6,2],5,2);
+    console.log(numArray);
+    let pairs = [ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]];
+    let obj = _.fromPairs(pairs);
+    console.log(obj);
     res.send('Hello there!');
 });
 module.exports = router;
