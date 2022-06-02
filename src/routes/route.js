4d981531dd93f2dcb4c08487ebdcfebc486c95f0
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-02 16:12:22 +0530

    assignment-4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const _ = require("lodash");
+const underscore = require("underscore");
 const externalModule = require('../logger/logger')
 const helperModule = require("../util/helper.js")
 const formatterModule = require("../validator/formatter.js")
@@ -14,5 +16,15 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/hello', function (req, res) {
+    let firstElement = underscore.first(["s","a","c"]);
+    console.log(firstElement);
+    let arr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let monthsArray = _.chunk(arr, 4);
+    console.log(monthsArray);
+    let oddArray = _.tail([1,3,5,7,9,11,13,15,17,19]);
+    console.log(oddArray);
+    res.send('Hello there!');
+});
 module.exports = router;
 // adding this comment for no reason
