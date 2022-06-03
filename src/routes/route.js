3859aeafb25a4368c6b3f94eb475c558c9a7f117
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-03 13:07:32 +0530

    assignment params

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,19 +9,21 @@ router.get('/movies', function(req, res){
     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
     res.send(movies);
 })
+//router.get('/movies/:indexNumber', function(req, res){
+    //let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    //let i = req.params.indexNumber;
+    //res.send(movies[i]);
+//})
 router.get('/movies/:indexNumber', function(req, res){
     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
-    let i = req.params.indexNumber;
-    res.send(movies[i]);
-})
-router.get('/movies/:indexNumber', function(req, res){
-    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
-    let i = req.params.indexNumber;
-    if(i <=3){
-    res.send(movies[i]);
+    let movieIndex = req.params.indexNumber;
+    let finalMovies = "";
+    if(movieIndex <movies.length){
+    finalMovies = movies[movieIndex];
     }else{
-    res.send("index not valid");
+    finalMovies= ("please enter number below "+movies.length);
     }
+    res.send(finalMovies);
 })
 module.exports = router;
 // adding this comment for no reason
