0f6dc5f3c21f3c491ab29c36ac11b521894edf46
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-03 10:17:18 +0530

    api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,13 +6,22 @@ const router = express.Router();
 
 
 router.get('/movies', function(req, res){
-    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    res.send(movies)
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    res.send(movies);
 })
 router.get('/movies/:indexNumber', function(req, res){
-    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    res.send(movies[i])
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    let i = req.params.indexNumber;
+    res.send(movies[i]);
 })
-
+router.get('/movies/:indexNumber', function(req, res){
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    let i = req.params.indexNumber;
+    if(i <=3){
+    res.send(movies[i]);
+    }else{
+    res.send("index not valid");
+    }
+})
 module.exports = router;
 // adding this comment for no reason
