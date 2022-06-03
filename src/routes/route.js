5cf32f2c3afc0ac64a400977273e559a85635acb
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-04 00:00:09 +0530

    assignment-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
 
 const router = express.Router();
-
 
 router.get('/movies', function(req, res){
     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
     res.send(movies);
 })
-//router.get('/movies/:indexNumber', function(req, res){
-    //let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
-    //let i = req.params.indexNumber;
-    //res.send(movies[i]);
-//})
+
+router.get('/movies/:indexNumber', function(req, res){
+    let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    let i = req.params.indexNumber;
+    res.send(movies[i]);
+})
+
 router.get('/movies/:indexNumber', function(req, res){
     let movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
     let movieIndex = req.params.indexNumber;
@@ -24,6 +23,51 @@ router.get('/movies/:indexNumber', function(req, res){
     finalMovies= ("please enter number below "+movies.length);
     }
     res.send(finalMovies);
+})
+
+router.get('/films', function(req,res){
+    let filmsArray = [ {
+        'id': 1,
+        'name': 'The Shining'}, 
+        {
+        'id': 2,
+        'name': 'Incendies'}, 
+        {
+        'id': 3,
+        'name': 'Rang de Basanti'}, 
+        {
+        'id': 4,
+        'name': 'Finding Nemo'}
+    ]
+    res.send(filmsArray);
+})
+
+let filmsArray = [ {
+    'id': 1,
+    'name': 'The Shining'}, 
+    {
+    'id': 2,
+    'name': 'Incendies'}, 
+    {
+    'id': 3,
+    'name': 'Rang de Basanti'}, 
+    {
+    'id': 4,
+    'name': 'Finding Nemo'}
+]
+
+router.get('/films/:filmId', function(req,res){
+    let filmIndex = req.params.filmId;
+    let index,film;
+    for(index=0;index<filmsArray.length;index++){
+        if(filmsArray[index].id==filmIndex){
+            film = (filmsArray[index].name);
+        }
+    }
+    if(filmIndex>=filmsArray.length){
+        film = ("index does not exists");
+    }
+    res.send(film);
 })
 module.exports = router;
 // adding this comment for no reason
