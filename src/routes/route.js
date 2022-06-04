fbc06f4ba17d4f09e53b3a73d4b3648c688a81d1
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-04 13:43:35 +0530

    JASON

--- src/routes/route.js
+++ src/routes/route.js
@@ -42,21 +42,22 @@ router.get("/test-api-6" , function(req, res) {
 })
 
 router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
+    res.send([ 23, 45 , 64, 55])
 })
 
 
 router.post("/test-post-2", function(req, res) {
     res.send(  { msg: "hi" , status: true }  )
+    console.log(req.body)
 })
 
 router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
+    let id = req.body.user
+    let pwd= req.body.password
 
-    // console.log( id , pwd)
+    console.log( id , pwd)
 
-    console.log( req.body )
+    //console.log( req.body )
 
     res.send(  { msg: "hi" , status: true }  )
 })
@@ -64,10 +65,61 @@ router.post("/test-post-3", function(req, res) {
 
 
 router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
+    let arr= [ 12, "functionup", false]
     let ele= req.body.element
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+let players =[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ],
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ],
+    },
+]
+
+router.post('/players', function (req, res) {
+    let playerName = req.body.name;
+    let playerData = req.body;
+    let nameRepeated = false;
+    let newData;
+    for(let i=0;i<players.length;i++){
+        if(players[i].name==playerName){
+            nameRepeated = true;
+            break; 
+        }
+    }
+    if(nameRepeated==false){
+        players.push(playerData);
+        newData =  { data: players , status: true };
+    }else{
+        newData = { data: players , status: true };
+    }
+    res.send(newData);
+})
+
 
 module.exports = router;
