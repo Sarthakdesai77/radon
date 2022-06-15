7e66c473c5436fab9536353493a7c550312cd1b9
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-06-15 20:58:51 +0530

    assignment/auth-2

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect(
+    'mongodb+srv://sarthak:K3rhbWfAU10MCbSA@cluster0.ne6rr.mongodb.net/sarthakdesai?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log('MongoDb is connected'))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use('/', route)
-
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
 });
